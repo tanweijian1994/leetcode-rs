7511c69d50fa99f75affa76ba65fbde14ba682bb
tanweijian <tanweijian199412@gmail.com>
2020-08-27 10:28:37 +0800

    整理一下题目

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -4,4 +4,4 @@ mod s0001_two_sum;
 mod s0002_add_two_numbers;
 mod s0003_longest_substring_without_repeating_characters;
 mod s0004_median_of_two_sorted_arrays;
-mod s00005_longest_palindromic_substring;
+mod s0005_longest_palindromic_substring;
--- src/solution/s0001_two_sum.rs
+++ src/solution/s0001_two_sum.rs
@@ -1,19 +1,17 @@
-/*
- * 给定一个整数数组 nums 和一个目标值 target，
- * 请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- *
- * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
- *
- * 示例:
- *
- * 给定 nums = [2, 7, 11, 15], target = 9
- * 因为 nums[0] + nums[1] = 2 + 7 = 9
- * 所以返回 [0, 1]
-*/
-
 use crate::solution::Solution;
 
 impl Solution {
+    /// 给定一个整数数组`nums`和一个目标值`target`，
+    /// 请你在该数组中找出和为目标值的那`两个`整数，并返回他们的`数组下标`。
+    /// 
+    /// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+    /// 
+    /// ## 示例:
+    /// ```
+    /// 给定 nums = [2, 7, 11, 15], target = 9
+    /// 因为 nums[0] + nums[1] = 2 + 7 = 9
+    /// 所以返回 [0, 1]
+    /// ```
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         use std::collections::BTreeMap;
         let mut bmap: BTreeMap<i32, usize> = BTreeMap::new();
@@ -24,7 +22,7 @@ impl Solution {
                 let j = *bmap.get(key).unwrap();
                 result.insert(0, j as i32);
                 result.insert(1, i as i32);
-                break;
+                break
             }
             bmap.insert(v, i);
         }

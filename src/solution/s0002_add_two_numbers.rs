d5f2b4e6a28ab17d4703fa6a0dec4541a2f5cbf6
tanweijian <tanweijian199412@gmail.com>
2020-08-28 00:11:34 +0800

    fmt + 第三题

--- src/solution/s0002_add_two_numbers.rs
+++ src/solution/s0002_add_two_numbers.rs
@@ -5,16 +5,19 @@ impl Solution {
     /// 给出两个`非空`的链表用来表示两个`非负`的整数。
     /// 其中，它们各自的位数是按照`逆序`的方式存储的，并且它们的每个节点只能存储`一位`数字。
     /// 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
-    /// 
+    ///
     /// 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
-    /// 
+    ///
     /// ## 示例：
     /// ```
     /// 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
     /// 输出：7 -> 0 -> 8
     /// 原因：342 + 465 = 807
     /// ```
-    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
         let (mut l1, mut l2) = (l1, l2);
         let mut dummy_head = Some(Box::new(ListNode::new(0)));
         let mut tail = &mut dummy_head;
@@ -61,15 +64,24 @@ impl Solution {
 #[test]
 fn add_two_numbers_test() {
     assert_eq!(
-        Solution::add_two_numbers(ListNode::vec_to_linked_list(vec![2, 4, 3]), ListNode::vec_to_linked_list(vec![5, 6, 4])),
+        Solution::add_two_numbers(
+            ListNode::vec_to_linked_list(vec![2, 4, 3]),
+            ListNode::vec_to_linked_list(vec![5, 6, 4])
+        ),
         ListNode::vec_to_linked_list(vec![7, 0, 8])
     );
     assert_eq!(
-        Solution::add_two_numbers(ListNode::vec_to_linked_list(vec![1, 2, 3]), ListNode::vec_to_linked_list(vec![8, 9, 1])),
+        Solution::add_two_numbers(
+            ListNode::vec_to_linked_list(vec![1, 2, 3]),
+            ListNode::vec_to_linked_list(vec![8, 9, 1])
+        ),
         ListNode::vec_to_linked_list(vec![9, 1, 5])
     );
     assert_eq!(
-        Solution::add_two_numbers(ListNode::vec_to_linked_list(vec![6, 4, 8]), ListNode::vec_to_linked_list(vec![5, 6, 9])),
+        Solution::add_two_numbers(
+            ListNode::vec_to_linked_list(vec![6, 4, 8]),
+            ListNode::vec_to_linked_list(vec![5, 6, 9])
+        ),
         ListNode::vec_to_linked_list(vec![1, 1, 8, 1])
     );
 }
--- src/solution/s0003_longest_substring_without_repeating_characters.rs
+++ src/solution/s0003_longest_substring_without_repeating_characters.rs
@@ -6,25 +6,57 @@ impl Solution {
     /// ## 示例 1:
     /// ```
     /// 输入: "abcabcbb"
-    /// 输出: 3 
+    /// 输出: 3
     /// 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3.
     /// ```
-    /// 
+    ///
     /// ## 示例 2:
     /// ```
     /// 输入: "bbbbb"
     /// 输出: 1
     /// 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
     /// ```
-    /// 
+    ///
     /// ## 示例 3:
     /// ```
     /// 输入: "pwwkew"
     /// 输出: 3
     /// 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
-    ///      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+    ///      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串.
     /// ```
     pub fn length_of_longest_substring(s: String) -> i32 {
-        0_i32
-    } 
+        let chars: Vec<char> = s.chars().collect();
+        let (mut start, mut end, mut max) = (0, 0, 0);
+
+        while end < chars.len() {
+            for idx in start..end {
+                if chars[idx] == chars[end] {
+                    start = idx + 1;
+                    break;
+                }
+            }
+            let len = end - start + 1;
+            if len > max {
+                max = len
+            }
+            end += 1
+        }
+        max as i32
+    }
+}
+
+#[test]
+fn length_of_longest_substring_test() {
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("abcabcbb")),
+        3
+    );
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("bbbbb")),
+        1
+    );
+    assert_eq!(
+        Solution::length_of_longest_substring(String::from("pwwkew")),
+        3
+    );
 }
